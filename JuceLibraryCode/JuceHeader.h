d07c0a4a6f1e570ce54eeee9bea7744425699255
AJ <ajjacobs@ualberta.ca>
2023-05-06 09:36:09 -0600

    minor projucer modifications

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -42,7 +42,7 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "FretboardQuiz";
-    const char* const  companyName    = "";
+    const char* const  companyName    = "ajjacobs";
     const char* const  versionString  = "1.0.0";
     const int          versionNumber  = 0x10000;
 }
